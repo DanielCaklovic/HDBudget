8388ed5534ff1958d3066ae3f3d47914ca8190fe
Daniel Čaklović <daniel.caklovic@gmail.com>
2021-01-09 16:24:23 +0100

    testing appsettings config

---
+++ HDBudget/Settings/Settings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HDBudget
+{
+    public class Settings
+    {
+        public String ApiUrl { get; set; }
+    }
+}
